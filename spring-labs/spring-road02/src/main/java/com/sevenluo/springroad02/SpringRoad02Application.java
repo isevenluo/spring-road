216c6da13934e4882e57183d3f543ab262e7e099
sevenluo <coderluojust@163.com>
2021-07-16 08:17:06 +0800

    spring-road02

---
+++ spring-labs/spring-road02/src/main/java/com/sevenluo/springroad02/SpringRoad02Application.java
@@ -0,0 +1,13 @@
+package com.sevenluo.springroad02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoad02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRoad02Application.class, args);
+    }
+
+}
---
+++ spring-labs/spring-road02/src/test/java/com/sevenluo/springroad02/SpringRoad02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevenluo.springroad02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRoad02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
